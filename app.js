70bad146249364269bb18ec40f4e7c6f24ca4d43
DatPham124 <phof1242003@gmail.com>
2024-08-28 19:46:57 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route.js");
+const ApiError = require("./app/api-error.js")
+
 
 const app = express();
 
@@ -9,6 +11,21 @@ app.use(express.json());
 
 app.get("/", (req,res) => {
     res.json({message: "Welcome to contact book application."});
+});
+
+// handle 404 response
+app.use((req, res, next) => {
+    // Code o day se chay khi khong co route duoc dinh nghia nao
+    // khop voi yeu cau. Goi next() de chuyen sang middleware xu ly loi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) =>{
+    // Middleware xu ly loi tap trung.
+    // Trong cac doan code xu ly o cac route, goi next(error) se chuyen ve middleware xu ly loi nay
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
 
 app.use("/api/contacts", contactsRouter);
--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,28 +1,35 @@
 exports.create = (req, res) => {
     res.send({ message: "create handler"});
+    next(err);
 };
 
 exports.findAll = (req, res) => {
     res.send({message: "findAll handler"});
+    next(err);
 };
 
 exports.findOne = (req, res) => {
     res.send({message: "findOne handler"});
+    next(err);
 };
 
 exports.update = (req, res) => {
     res.send({message: "update handler"});
+    next(err);
 };
 
 exports.delete = (req, res) => {
     res.send({message: "delete handler"});
+    next(err);
 }
 
 exports.deleteAll = (req, res) => {
     res.send({message: "deleteAll handler"});
+    next(err);
 };
 
 exports.findAllFavorite = (req, res) => {
     res.send({message: "findAllFavorite handler"});
+    next(err);
 };
 
