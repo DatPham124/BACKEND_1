34a937b68d98a838bc37663088cf057c913391f5
DatPham124 <phof1242003@gmail.com>
2024-09-12 13:59:28 +0700

    Lab2 xong bai 1

---
+++ app/services/contact.service.js
@@ -0,0 +1,10 @@
+const { ObjectID } = require("mongodb");
+
+class ContactService {
+    constructor(client){
+        this.Contact = client.db().collection("contacts");
+    }
+    // Dinh nghia cac phuong thuc truy xuat CSDL su dung mongodb API
+}
+
+module.exports = ContactService;
